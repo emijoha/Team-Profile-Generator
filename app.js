02cfed1e1760062456898002c815cedefe849e98
emi <ejhartline@gmail.com>
2020-04-18 18:16:51 -0400

    reworked createTeam function: divided it into seperate functions for each employee type

--- app.js
+++ app.js
@@ -10,134 +10,117 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+const employees = [];
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-const memberQs = [
-    {
-        type: "list",
-        name: "role",
-        message: "Which role does this team member hold?",
-        choices: ["Manager", "Engineer", "Intern"]
-    },
-    {
-        type: "input",
-        name: "name",
-        message: "What is their name?"
-    },
-    {
-        type: "input",
-        name: "id",
-        message: "What is their ID?"
-    },
-    {
-        type: "input",
-        name: "email",
-        message: "What is their email?"
-    },
-    {
-        type: "number",
-        name: "officeNumber",
-        message: "What is their office number?",
-        when: (answers) => answers.role === "Manager"
-    },
-    {
-        type: "input",
-        name: "github",
-        message: "What is their github username?",
-        when: (answers) => answers.role === "Engineer"
-    },
-    {
-        type: "input",
-        name: "school",
-        message: "What school do they attend?",
-        when: (answers) => answers.role === "Intern"
-    },
-    {
-        type: "confirm",
-        name: "addMember",
-        message: "Would you like to add another team member?",
-        default: true
-    }
-];
+const startTeam = () => {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is the manager's name?"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is their ID?"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is their email?"
+        },
+        {
+            type: "number",
+            name: "officeNumber",
+            message: "What is their office number?"
+        }
+    ]).then(answers => {
+    const newManager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
+    employees.push(newManager);
+    addTeam();
+    });
+};
 
-const makeTeamMember = () => {
-    return inquirer
-        .prompt(memberQs)
-        .then(answers => {
-            if (answers.role === "Manager") {
-                const newManager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
-                team.push(newManager);
-                if (answers.addMember === true) {
-                    makeTeamMember();
-                } else {
-                    const teamJSON = JSON.stringify(team);
-                    return console.log(`Your Team: \n${teamJSON}`);
-                }
-            }
-            else if (answers.role === "Engineer") {
-                const newEngineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
-                team.push(newEngineer);
-                if (answers.addMember === true) {
-                    makeTeamMember();
-                } else {
-                    const teamJSON = JSON.stringify(team);
-                    return console.log(`Your Team: \n${teamJSON}`);
-                }
-            }
-            else if (answers.role === "Intern") {
-                const newIntern = new Intern(answers.name, answers.id, answers.email, answers.school);
-                team.push(newIntern);
-                if (answers.addMember === true) {
-                    makeTeamMember();
-                } else {
-                    const teamJSON = JSON.stringify(team);
-                    return console.log(`Your Team: \n${teamJSON}`);
-                }
-            }
-            // incase the user is able to skip questions without selecting a role option
-            else {
-                console.log("Member's role was not selected");
-                makeTeamMember();
-            }
-        });
-}
+const createEngineer = () => {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is the engineer's name?"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is their ID?"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is their email?"
+        },
+        {
+            type: "number",
+            name: "github",
+            message: "What is their github username?"
+        }
+    ]).then(answers => {
+    const newEngineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
+    employees.push(newEngineer);
+    addTeam();
+    });
+};
 
-const team = [];
+const createIntern = () => {
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is the interns's name?"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is their ID?"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is their email?"
+        },
+        {
+            type: "number",
+            name: "school",
+            message: "What school do they attend?"
+        }
+    ]).then(answers => {
+    const newIntern = new Intern(answers.name, answers.id, answers.email, answers.school);
+    employees.push(newIntern);
+    addTeam();
+    });
+};
 
-makeTeamMember();
+const addTeam = () => {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "addTeam",
+            message: "Are there more team members to add?",
+            choices: ["Yes, add an Engineer", "Yes, add an Intern", "No, I'm done!"]
+        }
+    ]).then(answers => {
+        if (answers.addTeam === "Yes, add an Engineer") {
+            createEngineer();
+        }
+        if (answers.addTeam === "Yes, add an Intern") {
+            createIntern();
+        }
+        if (answers.addTeam === "No, I'm done!") {
+            fs.writeFile(outputPath, render(employees), err => {
+                if (err) throw err;
+                console.log("File team.html saved!");
+            })
+        }
+    });
+};
 
-// const makeTeamMember = answers => {
-//     if (answers.role === "Manager") {
-//         const newManager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
-//         team.push(newManager);
-//     } 
-//     else if (answers.role === "Engineer") {
-//         const newEngineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
-//         team.push(newEngineer);
-//     }
-//     else if (answers.role === "Intern") {
-//         const newIntern = new Intern(answers.name, answers.id, answers.email, answers.school);
-//         team.push(newIntern);
-//     }
-// }
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+startTeam();
