58c2eb6b01242b42c365488d689f8fdb2f55ac2b
emi <ejhartline@gmail.com>
2020-04-19 13:58:46 -0400

    Commenting added to subclasses js code, and to app.js code. Also added commenting in htmlRenderer starter code for better understanding in future.

--- app.js
+++ app.js
@@ -5,14 +5,19 @@ const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
 
+// Define starting path for team.html
 const OUTPUT_DIR = path.resolve(__dirname, "output");
+// final path of team.html
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+// Array to hold all employee objects
 const employees = [];
 
+// main function that starts app
 const startTeam = () => {
+    // Starts with prompting for Manager info, since all teams have a lead
     inquirer.prompt([
         {
             type: "input",
@@ -35,12 +40,16 @@ const startTeam = () => {
             message: "What is their office number?"
         }
     ]).then(answers => {
-    const newManager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
-    employees.push(newManager);
-    addTeam();
+        // answers are used to create an employee object with the Manager subclass
+        const newManager = new Manager(answers.name, answers.id, answers.email, answers.officeNumber);
+        // employee object pushed to employees array
+        employees.push(newManager);
+        // function to add more team members is called
+        addTeam();
     });
 };
 
+// When addTeam is call, if Engineer is selected, this createEngineer function will be called
 const createEngineer = () => {
     inquirer.prompt([
         {
@@ -64,12 +73,16 @@ const createEngineer = () => {
             message: "What is their github username?"
         }
     ]).then(answers => {
-    const newEngineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
-    employees.push(newEngineer);
-    addTeam();
+        // answers are used to create an employee object with the Engineer subclass
+        const newEngineer = new Engineer(answers.name, answers.id, answers.email, answers.github);
+        // employee object pushed to employees array
+        employees.push(newEngineer);
+        // addTeam is called to prompt user about additional team members or if done
+        addTeam();
     });
 };
 
+// When addTeam is call, if Intern is selected, this createIntern function will be called
 const createIntern = () => {
     inquirer.prompt([
         {
@@ -93,13 +106,18 @@ const createIntern = () => {
             message: "What school do they attend?"
         }
     ]).then(answers => {
-    const newIntern = new Intern(answers.name, answers.id, answers.email, answers.school);
-    employees.push(newIntern);
-    addTeam();
+        // answers are used to create an employee object with the Intern subclass
+        const newIntern = new Intern(answers.name, answers.id, answers.email, answers.school);
+        // employee object pushed to employees array
+        employees.push(newIntern);
+        // addTeam is called to prompt user about additional team members or if done
+        addTeam();
     });
 };
 
+// the addTeam function, first called at the end of the prompt in startTeam
 const addTeam = () => {
+    // one simple team-building prompt
     inquirer.prompt([
         {
             type: "list",
@@ -108,6 +126,7 @@ const addTeam = () => {
             choices: ["Yes, add an Engineer", "Yes, add an Intern", "No, I'm done!"]
         }
     ]).then(answers => {
+        // different functions called depending on the answer selected above
         if (answers.addTeam === "Yes, add an Engineer") {
             createEngineer();
         }
@@ -115,6 +134,8 @@ const addTeam = () => {
             createIntern();
         }
         if (answers.addTeam === "No, I'm done!") {
+            // if user is done adding engineers and interns, team.html file will be written
+            // final employees array is passed into the render() function from htmlRenderer.js, which define the content of the team.html file
             fs.writeFile(outputPath, render(employees), err => {
                 if (err) throw err;
                 console.log("File team.html saved!");
@@ -123,4 +144,5 @@ const addTeam = () => {
     });
 };
 
+// Initiates the app
 startTeam();
